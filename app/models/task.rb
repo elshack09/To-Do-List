42832520491564e02ef6864dbde20f38459e58db
elshack09 <elshack09@gmail.com>
2018-07-10 10:50:05 -0400

    created task routes

---
+++ app/models/task.rb
@@ -0,0 +1,7 @@
+class Task < ApplicationRecord
+    
+
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
   get 'welcome/index'
   # go to the index controller on the method
   root 'welcome#index' 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
