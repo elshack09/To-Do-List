d077177875a47036250d26411896e41ad22817bf
elshack09 <elshack09@gmail.com>
2018-07-10 14:36:07 -0400

    added update route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,31 @@
 class TasksController < ApplicationController
-  def index
-      @tasks =[]
-  end
-  
-  def new
-  end
+def index
+  @tasks = Task.all
+end
+
+def new
+  p @task = Task.new
+end
+
+def create
+  @task = Task.new(task_params)
+  @task.save
+    redirect_to tasks_path
+end
+
+def edit
+  @task = Task.find(params[:id])
+end
+
+def update
+  @task = Task.find(params[:id])
+  @task.update(task_params)
+  redirect_to tasks_path
+
+end
+
+private
+def task_params
+  params.require(:task).permit(:title, :details)
+end
 end
